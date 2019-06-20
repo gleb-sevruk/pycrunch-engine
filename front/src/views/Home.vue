096e7a0a68126fe0abd0401ddee9a0c0c601a59b
Gleb Sevruk <gsev@diceus.com>
2019-06-21 02:12:48 +0300

    Simplified structure of discovery response

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,30 +1,28 @@
 <template>
   <div class="home">
     <hr/>
-    <pc-socket-test @pipeline="on_pipeline"/>
+    <pc-socket-test @pipeline="on_socket_event"/>
     <div class="files ml-5">
+      <div class="text-secondary mb-4">Folder: {{folder}}</div>
       <div class="text-secondary">Discovered tests:</div>
       <template v-if="discovery_response">
-        <div class="files__single" v-for="module in discovery_response.modules" :key="module.filename">
-          <code class="text-info">{{module.filename}}</code>
+        <div class="files__single" v-for="test in discovery_response.tests" :key="test.fqn">
           <div class="test-methods">
-            <div class="test-methods ml-4" v-for="test in module.tests_found" :key="test.name + test.module">
               <div class="single__test d-flex mt-2" @click="run_test(test)">
-                <div class="alert-secondary px-2 pointer"> Pending </div>
-                <div class="single-test__name ml-2">{{test.name}}</div>
+                <div class="alert-secondary px-2 pointer text-capitalize single-test__badge"> {{test.state}} </div>
+                <div class="single-test__name ml-2">{{test.module}} - <span class="text-info">{{test.name}}</span></div>
               </div>
-            </div>
           </div>
         </div>
         <el-button class="mt-5" @click="run_all_tests()">Run all</el-button>
       </template>
     </div>
-<!--    <el-button @click="run_coverage()">Run</el-button>-->
     <div class="test-run ml-3">
 <!--      {{test_run}}-->
       <div class="all-files" v-if="test_run" >
         <div class="test-run__file mt-4" v-for="run in test_run.all_runs" :key="run.entry_point">
           <code>{{run.time_elapsed}}ms | {{run.entry_point}}</code>
+          <div class="terminal-output">{{run.captured_output}}</div>
           <div class="test-run__file mt-4 pb-5" v-for="file in run.files" :key="file.filename">
             <div class="file-name">{{file.filename}}</div>
             <pc-code-viewer :filename="file.filename" :coverage="file.lines_covered"></pc-code-viewer>
@@ -40,7 +38,7 @@
   import axios from 'axios'
   import config from '@/config'
   import CodeViewer from '../components/code-viewer.component'
-  import PcSocketTest from './PcSocketTest'
+  import PcSocket from './PcSocket'
 
   export default {
   name: 'home',
@@ -56,7 +54,7 @@
     }
   },
   components: {
-    'pc-socket-test' : PcSocketTest,
+    'pc-socket-test' : PcSocket,
     'pc-code-viewer' : CodeViewer,
   },
   async mounted () {
@@ -72,22 +70,27 @@
     }
   },
   methods: {
-    async run_coverage () {
-      let x = await axios.post(config.api_url + '/coverage', {entry_files: [ '/Users/gleb/code/PyCrunch/tests.py',
-          '/Users/gleb/code/PyCrunch/playground.py',]})
-      this.test_run = x.data
+    file_modification_event: function (data) {
+      let file = data.modified_file
+      let dependent_tests = this.dependencies[file]
+      if (dependent_tests) {
+        this.run_specified_tests(dependent_tests)
+      }
     },
-    on_pipeline (data){
+    test_run_completed (data) {
+      this.test_run = data.coverage
+      for (let fqn in this.test_run.all_runs) {
+        let test_in_list = this.discovery_response.tests.find(_ => _.fqn === fqn)
+        test_in_list.state = 'Completed'
+      }
+    },
+    on_socket_event (data){
       console.log('pipe', data)
       if (data.event_type === 'file_modification') {
-        let file = data.modified_file
-        let dependent_tests = this.dependencies[file]
-        if (dependent_tests) {
-          this.run_specified_tests(dependent_tests)
-        }
+        this.file_modification_event(data)
       }
       if (data.event_type === 'test_run_completed') {
-        this.test_run = data.coverage
+        this.test_run_completed(data)
       }
       if (data.event_type === 'combined_coverage_updated') {
         this.combined_coverage = data.combined_coverage
@@ -96,7 +99,7 @@
 
     },
     async run_test (test) {
-       await axios.post(config.api_url + '/run-tests', {entry_point:`${test.filename}:${test.name}`, tests: [ test ]})
+       await axios.post(config.api_url + '/run-tests', {tests: [ test ]})
       // this.test_run = x.data
     },
     async run_all_tests () {
@@ -104,11 +107,12 @@
 
     },
     async run_specified_tests (tests) {
-      await axios.post(config.api_url + '/run-tests', {entry_point:`--all--`, tests })
+      await axios.post(config.api_url + '/run-tests', { tests })
 
     },
     get_all_tests () {
-      let xx = this.discovery_response.modules.flatMap(_ => _.tests_found)
+      // let xx = this.discovery_response.modules.flatMap(_ => _.tests_found)
+      let xx = this.discovery_response.tests
       return xx
     }
   },
@@ -125,5 +129,16 @@
 
   .pointer {
     cursor: pointer;
+  }
+
+  .single-test__badge {
+    width: 6em;
+    text-align: center;
+  }
+  .terminal-output{
+    white-space: pre;
+    background-color: #292c2f;
+    font-family: monospace;
+    color: white;
   }
 </style>
