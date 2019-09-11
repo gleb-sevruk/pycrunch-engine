c8cff03251866f11f2a086d261074b37ee25e607
Gleb Sevruk <gsev@diceus.com>
2019-09-11 21:33:54 +0300

    update version number

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ import setuptools
 from distutils.core import setup
 
 setup(name='pycrunch-engine',
-      version='0.1',
+      version='0.5',
       description='Automatic Test Runner Engine',
-      url='http://github.com/gleb-sevruk/pycrunch',
+      url='http://github.com/gleb-sevruk/pycrunch-engine',
       author='Gleb Sevruk',
-      author_email='sevruk.gleb@gmail.com',
-      license='MIT',
+      author_email='support@pycrunch.com',
+      license='libpng',
       packages=setuptools.find_packages(),
+      download_url='https://github.com/gleb-sevruk/pycrunch-engine/archive/v_05.tar.gz',
       setup_requires=['wheel'],
       entry_points={
           'console_scripts': ['pycrunch-engine=pycrunch.main:run'],
