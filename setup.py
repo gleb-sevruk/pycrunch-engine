75dece2c5be84faf0b7089bb897e7bb2eba7dd27
Gleb Sevruk <gleb.sevruk@databand.ai>
2023-03-27 00:34:24 +0300

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 from distutils.core import setup
 
 setup(name='pycrunch-engine',
-      version='1.5',
+      version='1.5.0',
       description='Automatic Test Runner Engine',
       url='http://github.com/gleb-sevruk/pycrunch-engine',
       author='Gleb Sevruk',
