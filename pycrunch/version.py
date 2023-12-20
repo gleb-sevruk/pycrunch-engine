d268f8aea28b54461a462ddecbf5f1e853cd3afe
Gleb Sevruk <sevruk.gleb@gmail.com>
2023-12-21 00:50:39 +0200

    Downgrade version to 1.6.3 (since bumpversion was messing around)

--- pycrunch/version.py
+++ pycrunch/version.py
@@ -1,4 +1,4 @@
-version_info = dict(major=1, minor=6, patch=5)
+version_info = dict(major=1, minor=6, patch=3)
 version_info_str = (
     f"{version_info['major']}.{version_info['minor']}.{version_info['patch']}"
 )
